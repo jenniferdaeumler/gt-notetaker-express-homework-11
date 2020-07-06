0e2cb7d103784f8cf44bd72fdf5dc2fb3e3aaf29
jdaeumler <jenniferdaeumler@gmail.com>
2020-07-06 13:25:35 -0500

    more basic skeleton

---
+++ server.js
@@ -0,0 +1,18 @@
+//Require express
+const express = require("express");
+//Create instance of express called app
+const app = express();
+//Add a port 
+const PORT = process.env.PORT || 3000;
+
+//Add data processing for POST routes
+app.use(express.urlencoded){ extended:true }));
+app.use(express.json( )):
+
+
+
+//Listen on that port
+app.listen(PORT, (req,res) =>{
+    console.log(`Currently running on http://localhost:${PORT}`)
+});
+
