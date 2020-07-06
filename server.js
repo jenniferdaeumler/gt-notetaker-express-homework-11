83707b6c93bff2dcb1c271a70790f01bfca0a044
jdaeumler <jenniferdaeumler@gmail.com>
2020-07-06 15:32:03 -0500

    GET API route works, const created for db file, deleted return from two view routes

--- server.js
+++ server.js
@@ -17,15 +17,20 @@ app.use(express.static('public'));
 //View / HTML routes 
 // * GET `*` - Should return the `index.html` file
 app.get("/", (req, res) => {
-    return res.sendFile(path.join(__dirname, "./public/index.html"));
+     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 // * GET `/notes` - Should return the `notes.html` file.
 app.get("/notes", (req, res) => {
-    return res.sendFile(path.join(__dirname, "./public/notes.html"));
+     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 //API / JSON routes
 //GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+//Connect db.json file to server.js file
+const db = require("./db/db.json");
+app.get("/api/notes", function(req,res){
+    return res.json(db);
+})
 //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 //DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
