844dc1857ca8ad6a9885c7c60aad22bef7269cf2
jdaeumler <jenniferdaeumler@gmail.com>
2020-07-06 13:32:18 -0500

    Reconfig file so runs

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Add data processing for POST routes
-app.use(express.urlencoded){ extended:true }));
-app.use(express.json( )):
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//View / HTML routes
+app.get("/", (req,res) => {
+    res.send("Hello world");
+})
+
+//API / JSON routes
 
 
 //Listen on that port
